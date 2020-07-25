dbf4db1c86cd2281221e61c121886f5c257bc361
Andrea Laforgia <a.laforgia@gmail.com>
2020-07-25 20:49:28 +0100

    Add missing test

---
+++ src/test/java/tdd/huffman/ByteConverterTest.java
@@ -0,0 +1,33 @@
+package tdd.huffman;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ByteConverterTest {
+
+    private static byte[] ARRAY_OF_BYTES = new byte[] {
+            (byte) 0b10101010,
+            (byte) 0b01010101,
+            (byte) 0b00101000,
+            (byte) 0b00010000,
+            (byte) 0b10101010,
+            (byte) 0b01010101,
+            (byte) 0b00101000,
+            (byte) 0b00010000
+    };
+
+    private static final long LONG = -6172983662199101424L;
+
+    private ByteConverter byteConverter = new ByteConverter();
+
+    @Test
+    void shouldConvertLongToBytes() {
+        assertThat(byteConverter.longToBytes(LONG)).isEqualTo(ARRAY_OF_BYTES);
+    }
+
+    @Test
+    void shouldConvertBytesToLong() {
+        assertThat(byteConverter.bytesToLong(ARRAY_OF_BYTES)).isEqualTo(LONG);
+    }
+}
